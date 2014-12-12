7476404463d63dcf28622b104452a8e08bf08339
samrinamim <ssmim.phys@gmail.com>
2014-12-11 22:26:08 -0500

    array_diff_assoc() example

--- array_function/array.php
+++ array_function/array.php
@@ -1,13 +1,7 @@
 
 <?php
-function array_count_values_ci($array) {
-    $newArray = array();
-    foreach ($array as $values) {
-        if (!array_key_exists(strtolower($values), $newArray)) {
-            $newArray[strtolower($values)] = 0;
-        }
-        $newArray[strtolower($values)] += 1;
-    }
-    return $newArray;
-}
+$array1 = array("a" => "green", "b" => "brown", "c" => "blue", "red");
+$array2 = array("a" => "green", "yellow", "red");
+$result = array_diff_assoc($array1, $array2);
+print_r($result);
 ?>
