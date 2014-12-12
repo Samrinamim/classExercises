34b8cdf2e54fe10c6e1fa8d1b52790da4e5c2693
samrinamim <ssmim.phys@gmail.com>
2014-12-11 23:32:15 -0500

    array_keys() example

--- array_function/create_function9.php
+++ array_function/create_function9.php
@@ -1,7 +1,11 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: lict
- * Date: 12/11/14
- * Time: 11:26 PM
- */ 
+$array = array(0 => 100, "color" => "red");
+print_r(array_keys($array));
+
+$array = array("blue", "red", "green", "blue", "blue");
+print_r(array_keys($array, "blue"));
+
+$array = array("color" => array("blue", "red", "green"),
+    "size"  => array("small", "medium", "large"));
+print_r(array_keys($array));
+?
