ec32de261165544f84af6bb99a43c4c193da9772
samrinamim <ssmim.phys@gmail.com>
2014-12-11 22:33:29 -0500

    array_diff_uassoc() example

--- array_function/array.php
+++ array_function/array.php
@@ -1,8 +1,16 @@
 
 
 <?php
-$array1 = array('blue'  => 1, 'red'  => 2, 'green'  => 3, 'purple' => 4);
-$array2 = array('green' => 5, 'blue' => 6, 'yellow' => 7, 'cyan'   => 8);
+function key_compare_func($a, $b)
+{
+    if ($a === $b) {
+        return 0;
+    }
+    return ($a > $b)? 1:-1;
+}
 
-var_dump(array_diff_key($array1, $array2));
+$array1 = array("a" => "green", "b" => "brown", "c" => "blue", "red");
+$array2 = array("a" => "green", "yellow", "red");
+$result = array_diff_uassoc($array1, $array2, "key_compare_func");
+print_r($result);
 ?>
