9019c9cbcb9699de10ec0564d76c72ead8d631b3
samrinamim <ssmim.phys@gmail.com>
2014-12-11 23:55:38 -0500

    array_pad() example

--- array_function/create_function17.php
+++ array_function/create_function17.php
@@ -1,7 +1,13 @@
+
 <?php
-/**
- * Created by PhpStorm.
- * User: lict
- * Date: 12/11/14
- * Time: 11:49 PM
- */ 
+$input = array(12, 10, 9);
+
+$result = array_pad($input, 5, 0);
+// result is array(12, 10, 9, 0, 0)
+
+$result = array_pad($input, -7, -1);
+// result is array(-1, -1, -1, -1, 12, 10, 9)
+
+$result = array_pad($input, 2, "noop");
+// not padded
+?>
