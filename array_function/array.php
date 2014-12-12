1e657c3868c303ff5d7da0503e0161bcb0cf7de8
samrinamim <ssmim.phys@gmail.com>
2014-12-11 22:36:10 -0500

    array_diff_ukey() example

--- array_function/array.php
+++ array_function/array.php
@@ -1,16 +1,19 @@
 
 
+
 <?php
-function key_compare_func($a, $b)
+function key_compare_func($key1, $key2)
 {
-    if ($a === $b) {
+    if ($key1 == $key2)
         return 0;
-    }
-    return ($a > $b)? 1:-1;
+    else if ($key1 > $key2)
+        return 1;
+    else
+        return -1;
 }
 
-$array1 = array("a" => "green", "b" => "brown", "c" => "blue", "red");
-$array2 = array("a" => "green", "yellow", "red");
-$result = array_diff_uassoc($array1, $array2, "key_compare_func");
-print_r($result);
+$array1 = array('blue'  => 1, 'red'  => 2, 'green'  => 3, 'purple' => 4);
+$array2 = array('green' => 5, 'blue' => 6, 'yellow' => 7, 'cyan'   => 8);
+
+var_dump(array_diff_ukey($array1, $array2, 'key_compare_func'));
 ?>
